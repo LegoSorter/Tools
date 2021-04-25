502e504f1484e029da50e2d0e87d83afac3c3574
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-04-25 06:55:51 +0200

    not sort names in a group

--- grab_dataset.py
+++ grab_dataset.py
@@ -9,7 +9,6 @@ def get_names_in_groups(file_name):
     with open(file_name) as file:
         for line in file:
             groups.append([name.strip() for name in line.split(' ') if len(name.strip()) > 0])
-            groups[-1].sort()
 
     return groups
 
